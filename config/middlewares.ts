16b091ded16fa4ebf57c1c994e8c282e529fad3f
shahzaib Shahid <110985745+shahzaib-191@users.noreply.github.com>
2025-04-23 10:09:35 +0500

    Update middlewares.ts

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,8 +1,16 @@
 export default [
   'strapi::logger',
   'strapi::errors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:3000', 'https://mediadunes.com'],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      credentials: true,
+    },
+  },
   'strapi::security',
-  'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
